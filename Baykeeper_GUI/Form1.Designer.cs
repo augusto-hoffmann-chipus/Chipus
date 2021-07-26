d71cf4c9f4ed4cdafab164fd7b3a6c2b221b6848
Augusto Hoffmann <augusto.hoffmann@chipus.com.br>
2021-07-26 17:10:14 -0300

    The start of the project.

---
+++ Baykeeper_GUI/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿
+namespace Baykeeper_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_serialPorts = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox_serialPorts
+            // 
+            this.comboBox_serialPorts.FormattingEnabled = true;
+            this.comboBox_serialPorts.Location = new System.Drawing.Point(667, 12);
+            this.comboBox_serialPorts.Name = "comboBox_serialPorts";
+            this.comboBox_serialPorts.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_serialPorts.TabIndex = 0;
+            this.comboBox_serialPorts.Click += new System.EventHandler(this.comboBox_serialPorts_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox_serialPorts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_serialPorts;
+    }
+}
+
---
+++ Baykeeper_GUI/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Baykeeper_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox_serialPorts_Click(object sender, EventArgs e)
+        {
+            comboBox_serialPorts.Items.Clear();
+            String[] ports = SerialPort.GetPortNames();
+            comboBox_serialPorts.Items.AddRange(ports);
+        }
+    }
+}
